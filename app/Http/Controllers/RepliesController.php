b01edb7f66c7f74be8adbaf3bf40628d119fe2f6
marun <admin@runrun520.cn>
2018-12-04 05:53:49 +0000

    消息通知列表,回复的时候提醒作者

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ReplyRequest;
@@ -21,6 +22,9 @@ class RepliesController extends Controller
         $reply->user_id = Auth::id();
         $reply->topic_id = $request->topic_id;
         $reply->save();
+        $topic = $reply->topic;
+
+        $topic->user->notify(new TopicReplied($reply));
 
         return redirect()->to($reply->topic->link())->with('success', '回复创建成功！');
     }
