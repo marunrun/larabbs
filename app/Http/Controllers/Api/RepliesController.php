bde4ad86174dd4c36325ea43bef4cbc1cec9974b
marun <admin@runrun520.cn>
2018-12-04 05:21:22 +0000

    回复列表

--- app/Http/Controllers/Api/RepliesController.php
+++ app/Http/Controllers/Api/RepliesController.php
@@ -5,11 +5,43 @@ namespace App\Http\Controllers\Api;
 use App\Http\Requests\Api\ReplyRequest;
 use App\Models\Reply;
 use App\Models\Topic;
+use App\Models\User;
 use App\Transformers\ReplyTransformer;
 use Illuminate\Http\Request;
 
 class RepliesController extends Controller
 {
+
+    /**
+     * 话题回复列表
+     * @param Topic $topic
+     * @return \Dingo\Api\Http\Response
+     */
+    public function index(Topic $topic)
+    {
+        $replies = $topic->replies()->paginate(20);
+        return $this->response->paginator($replies , new ReplyTransformer());
+    }
+
+    /**
+     * 获取某个用户的回复列表
+     * @param User $user
+     * @return \Dingo\Api\Http\Response
+     */
+    public function userIndex(User $user)
+    {
+        $replies = $user->replies()->paginate(20);
+
+        return $this->response->paginator($replies, new ReplyTransformer());
+    }
+    
+    /**
+     * 发布回复
+     * @param ReplyRequest $request
+     * @param Topic $topic
+     * @param Reply $reply
+     * @return $this
+     */
     public function store(ReplyRequest $request , Topic $topic , Reply $reply)
     {
         $reply->content = $request->content;
@@ -19,5 +51,26 @@ class RepliesController extends Controller
 
         return $this->response->item($reply, new ReplyTransformer())
                 ->setStatusCode(201);
+    }
+
+    /**
+     * 删除某条回复
+     * @param Topic $topic
+     * @param Reply $reply
+     * @return \Dingo\Api\Http\Response|void
+     * @throws \Exception
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function destroy(Topic $topic , Reply $reply)
+    {
+        if ($topic->id != $reply->topic_id){
+            return $this->response->errorBadRequest();
+        }
+
+        $this->authorize('destroy',$reply);
+
+        $reply->delete();
+
+        return $this->response->noContent();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -59,6 +59,13 @@ $api->version('v1',[
         // 获取某个用户发布的话题
         $api->get('users/{user}/topics','TopicsController@userIndex')
             ->name('api.users.topics.index');
+        // 某个话题的回复列表
+        $api->get('topics/{topic}/replies','RepliesController@index')
+            ->name('api.topics.replies.index');
+        // 某个用户的回复列表
+        $api->get('users/{user}/replies','RepliesController@userIndex')
+            ->name('api.users.replies.index');
+
 
         // 需要token验证的接口
         $api->group(['middleware' => 'api.auth'],function ($api){
@@ -82,6 +89,9 @@ $api->version('v1',[
             // 发布回复
             $api->post('topics/{topic}/replies','RepliesController@store')
                 ->name('api.topics.replies.store');
+            // 删除回复
+            $api->delete('topics/{topic}/replies/{reply}','RepliesController@destroy')
+                ->name('api.topics.replies.destroy');
         });
 
     });
