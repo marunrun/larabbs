c8530247ea81904a7b028dd23deac6d6a87a4aec
marun <admin@runrun520.cn>
2018-12-04 06:17:59 +0000

    消息阅读

--- routes/api.php
+++ routes/api.php
@@ -98,6 +98,9 @@ $api->version('v1',[
             // 通知统计
             $api->get('user/notifications/stats','NotificationsController@stats')
                 ->name('api.user.notifications.stats');
+            // 标记通知为已读
+            $api->patch('user/read/notifications','NotificationsController@read')
+                ->name('api.user.notifications.read');
         });
 
     });
