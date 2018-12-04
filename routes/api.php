c39ae70358b116ba9bb940e57767787c78caa1f8
marun <admin@runrun520.cn>
2018-12-04 07:06:42 +0000

    当前用户的权限列表

--- routes/api.php
+++ routes/api.php
@@ -101,6 +101,9 @@ $api->version('v1',[
             // 标记通知为已读
             $api->patch('user/read/notifications','NotificationsController@read')
                 ->name('api.user.notifications.read');
+            // 获取当前登录用户权限
+            $api->get('user/permissions','PermissionsController@index')
+                ->name('api.user.permissions.index');
         });
 
     });
